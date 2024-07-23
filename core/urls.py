181ebb8a0ffc1def445c3ddcbaf11c4ec3deca30
Fabiano Freitas <fabiano.freitas@gmail.com>
2024-07-22 21:17:34 -0300

    projeto teste

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [    
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('class_/', views.class_, name='class_'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('contact/', views.contact, name='contact'),    
+    path('modal/', views.modal, name='modal'),
+]
---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def class_(request):
+    return render(request, "core/class.html")
+
+def schedule(request):
+    return render(request, "core/schedule.html")
+
+
+def contact(request):
+    return render(request, "core/contact.html")
+
+def modal(request):
+    return render(request, "core/modal.html")
+
+
